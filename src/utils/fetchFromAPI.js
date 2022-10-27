d4eb6d4f3b328f213ec7135f4006cca23fe08d9a
laynedesignsui <laynedesignsui@gmail.com>
2022-10-27 08:06:55 -0400

    first commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    "X-RapidAPI-Key": "3eff83de3dmsh851208f34b8d2efp1e1744jsncce8300b1d30",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
